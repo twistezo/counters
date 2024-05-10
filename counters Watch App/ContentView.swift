4ee86eddc35a75fce941467c3df3ec597e08c3a0
Łukasz Kółko <kolkolukasz@gmail.com>
2024-05-10 17:06:52 +0200

    xcode init

---
+++ counters Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  counters Watch App
+//
+//  Created by Łukasz on 10/05/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ counters Watch App/countersApp.swift
@@ -0,0 +1,17 @@
+//
+//  countersApp.swift
+//  counters Watch App
+//
+//  Created by Łukasz on 10/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct counters_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
